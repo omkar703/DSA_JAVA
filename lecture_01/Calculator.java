9f9e9ed02addcb59cb8f5239406e32045685c7cd
omkar703 <omkarphadatareofficial30122004@gmail.com>
2025-07-29 17:09:26 +0530

    Commit

---
+++ lecture_01/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Calculator{
+    public static void main(String[] args) {
+        // Input 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+    }
+}
---
+++ lecture_01/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        System.out.print("Hello, World with java \n"); // after the output is print then go to the next line /n
+        System.out.print("Hello, World! \n"); // This will print "Hello, World!" and move to the next line
+        System.out.println("hey there"); // This will print "Hello, World!" and move to the next line
+        System.out.print("*\n**\n***\n****\n*****\n******\n*******\n********\n*********\n**********\n"); // This will print a pattern of stars
+
+        // variables in java 
+        int age = 48;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isEmployed = true;
+        String name = "omkar";
+        System.out.println("Age: " + age);
+        // System.out.println(typeof(age)); // This will print the type of the variable age
+        System.out.println("Salary: " + salary);
+        
+    }
+}
