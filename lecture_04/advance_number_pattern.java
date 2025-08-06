120bd77b821e56fb46d9db7989fb99bf24fc255c
omkar703 <omkarphadatareofficial30122004@gmail.com>
2025-08-07 00:00:51 +0530

    edded

--- lecture_04/advance_number_pattern.java
+++ lecture_04/advance_number_pattern.java
@@ -14,3 +14,19 @@ class advance_number_pattern {
     }
    }
 }
+class advance_number_pattern {
+    //    *
+    //   **
+    //  ***
+    
+   public static void main(String[] args) {
+     for(int i = 1 ; i <= 4 ; i++){
+        for(int j = 1 ; j<= (4-j) ; i++){
+            System.out.print(" ");
+        }
+        for (int k = 1; k <= 4 ; k++){
+            System.out.print(" * ");
+        }
+    }
+   }
+}
